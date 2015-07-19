e8f81563c91e6fbb131b90a33d89ad20a04ba4c5
AmShaegar <developer@amshaegar.de>
2015-07-19 13:16:48 +0200

    added main frame

---
+++ src/main/java/swarm/Swarm.java
@@ -0,0 +1,29 @@
+package swarm;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class Swarm extends JFrame {
+
+	public Swarm() {
+		super();
+		initGUI();
+	}
+
+	private void initGUI() {
+		setSize(250, 200);
+		setTitle("Swarm");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+	}
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				Swarm app = new Swarm();
+				app.setVisible(true);
+			}
+		});
+	}
+
+}
