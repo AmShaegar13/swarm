4283a4f53a8508ff92fac3e0715489690412db7f
AmShaegar <developer@amshaegar.de>
2015-07-26 17:17:54 +0200

    make use of Java's native implementation for vector length

--- src/main/java/de/amshaegar/swarm/util/Vector2D.java
+++ src/main/java/de/amshaegar/swarm/util/Vector2D.java
@@ -4,6 +4,8 @@ import java.awt.geom.Point2D;
 
 public class Vector2D extends Point2D.Double {
 
+	private static final Vector2D ZERO = new Vector2D(0, 0);
+
 	public Vector2D(final double x, final double y) {
 		this.x = x;
 		this.y = y;
@@ -33,12 +35,8 @@ public class Vector2D extends Point2D.Double {
 		return new Vector2D(x / m, y / m);
 	}
 
-	public double lengthSq() {
-		return Math.pow(x, 2) + Math.pow(y, 2);
-	}
-
 	public double length() {
-		return Math.sqrt(lengthSq());
+		return distance(ZERO);
 	}
 
 	public Vector2D normalize() {
